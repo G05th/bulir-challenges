c5c6351c0601794756876e4f24d746fd3f5f344e
G05th <germanochita27@gmail.com>
2025-10-19 20:07:07 +0100

    refactor(safety): Fortalecimento da Integridade Transacional e CRUD
    Esta refatoração foca em mitigar riscos de segurança e garantir a integridade dos dados, com base nos testes rigorosos:
    
    - [x] **Risco de Saldo (ReservationService):** Implementada a correção para buscar o saldo do cliente *diretamente do banco de dados* antes de iniciar a transação. Isso resolve o risco de usar o saldo desatualizado do JWT, garantindo o princípio *Fail Fast*.
    - [x] **Integridade de Exclusão (ServiceService):** Adicionada a verificação de dependência no método DELETE. Lança **ConflictException (409)** se houver reservas ATIVAS associadas ao serviço, garantindo a integridade referencial.
    - [x] Confirmação de que a lógica de estorno ( Cliente /  Prestador) está correta e validada.

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ReservationModule } from './reservation/reservation.module';
+import { ServiceController } from './service/service.controller';
+import { ServiceModule } from './service/service.module';
+import { ServiceService } from './service/service.service';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, ReservationModule, ServiceModule, PrismaModule],
+  controllers: [AppController, ServiceController],
+  providers: [AppService, ServiceService],
 })
 export class AppModule {}
--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-
+import { JwtStrategy } from './strategy/jwt.strategy';
 @Module({
   imports: [
     PrismaModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret:
         'bff4f886ec71c1c15d44ae1f66354a6388d10c9682cacfe9f2f4360acb2a18c381f1721540a82374646de58e251a3121fde4df6db198b6b7a9b5faf9e630b349',
@@ -16,7 +18,7 @@ import { AuthService } from './auth.service';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
-  exports: [AuthService, AuthModule],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService, AuthModule, JwtModule],
 })
 export class AuthModule {}
