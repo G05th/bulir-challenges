a7d373f2b371e3a780756695890fdf2da8dfb767
G05th <germanochita27@gmail.com>
2025-10-19 20:36:51 +0100

    .env file

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -9,7 +10,15 @@ import { ServiceModule } from './service/service.module';
 import { ServiceService } from './service/service.service';
 
 @Module({
-  imports: [AuthModule, ReservationModule, ServiceModule, PrismaModule],
+  imports: [
+    AuthModule,
+    ReservationModule,
+    ServiceModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController, ServiceController],
   providers: [AppService, ServiceService],
 })
--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { PrismaModule } from '../prisma/prisma.module';
@@ -9,16 +10,21 @@ import { JwtStrategy } from './strategy/jwt.strategy';
   imports: [
     PrismaModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret:
-        'bff4f886ec71c1c15d44ae1f66354a6388d10c9682cacfe9f2f4360acb2a18c381f1721540a82374646de58e251a3121fde4df6db198b6b7a9b5faf9e630b349',
-      signOptions: {
-        expiresIn: '7d',
-      },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>(
+            'JWT_EXPIRES_IN',
+          ) as unknown as StringValue,
+        },
+      }),
     }),
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
-  exports: [AuthService, AuthModule, JwtModule],
+  exports: [AuthService, AuthModule, PassportModule],
 })
 export class AuthModule {}
